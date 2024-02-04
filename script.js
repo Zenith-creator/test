9323694125ea0652835ce280c52117e93cb5e04d
Zenith-creator <122456210+Zenith-creator@users.noreply.github.com>
2024-02-04 13:41:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+const empty = document.querySelector('.empty');
+
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+
+yesBtn.addEventListener('click', () => {
+    question.innerHTML = 'SWEET ;)';
+    empty.innerHTML='';
+
+});
+
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
